df707fdd53fbd5557d3634bddb70da4c9b6c8947
Andrew Humphries <andrewhumphrieswork@outlook.com>
2022-03-14 17:35:15 -0500

    setting up webpack and working with flickity

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/myScript.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Image Slider Project',
+      inject: 'body',
+      template: 'src/index.html',
     }),
   ],
 
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'images/[hash][ext][query]',
     clean: true,
   },
   module: {
@@ -19,6 +27,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
